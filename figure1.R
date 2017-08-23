d7f7e9c1b4ec02dcd8c5129fe5a127e8d1710c0a
Pan <wei-hung.pan@i-kmb.de>
2017-08-23 09:45:44 +0200

    finall version of figure1

--- figure1.R
+++ figure1.R
@@ -4,6 +4,7 @@
 
 library(ggplot2)
 library(gplots)
+library(stringr)
 
 #read data
 ALL_sign<-read.table("../figure1/significant gene express.txt",sep="\t",header=T)
@@ -16,7 +17,20 @@ DOWN<-which(ALL_sign$fold_change>1)[order(ALL_sign[which(ALL_sign$fold_change>1)
 
 ALL_sign[UP,"upwtest.intersect.up_sig..fc_sig.."]
 ALL_sign[DOWN,"upwtest.intersect.up_sig..fc_sig.."]
+
 #take out the value
+colnames(ALL_sign)[grepl("GCRMA",colnames(ALL_sign))]
+label_x<-gsub("(\\d+).*", "\\1", colnames(ALL_sign)[grepl("GCRMA",colnames(ALL_sign))])
+label_x<-gsub("NC","H",label_x)
+col_side<-(rep(c("darkgreen","orange"),each=10))
+
 GEvalue<-ALL_sign[,grepl("GCRMA",colnames(ALL_sign))]
-head(as.matrix(GEvalue))
-heatmap.2(as.matrix(GEvalue)[c(UP,DOWN),],scale="row",col = rev(redblue(20)),trace="none")
+GEvalue50<-as.matrix(GEvalue)[c(UP,DOWN),]
+GEvalue50<-t(apply(GE_value,1,scale))
+
+heatmap.2(GEvalue50,scale="row",col = rev(redblue(11)),trace="none",
+          labRow = ALL_sign$Gene.Symbol[c(UP,DOWN)],labCol = label_x,
+          ColSideColors = col_side,density.info="none")
+
+
+
